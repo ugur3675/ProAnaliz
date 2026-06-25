80829ba1341a7ed31404c5e5b47d0b993d507cc6
ugur3675 <uugurr3675@gmail.com>
2026-06-25 11:46:42 +0300

    main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from '../fiyatlama-pro'
+
+createRoot(document.getElementById('root')!).render(
+  React.createElement(App)
+)
